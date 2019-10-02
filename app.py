e8dc36938bdecdc9443ac4bf029a67d8182a45ca
aim2121 <55549983+aim2121@users.noreply.github.com>
2019-10-02 10:13:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
     userText = decoded['queryResult']['intent']['displayName']
-    if (ีuserText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
         sendText(user,'ไอ้บ้า')
     elif (userText == 'หิวมั้ย') :
         sendText(user,'สวยน่ะ')
